7190aab05c4d913ff67bb30ad28b0e7d955bfae4
Giovane Santos <giovane98sp@gmail.com>
2023-08-15 13:02:10 -0300

    add reorderQuestions function

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,10 +1,22 @@
+import { useContext } from 'react'
+
+import { QuizContext } from '../context/quiz'
+
 import './Questions.css'
 
 export const Questions = () => {
+  const [quizState, dispatch] = useContext(QuizContext)
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
+  
 
   return (
-    <div>
-      <h1>Questions</h1>
+    <div id='question'>
+      <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+      <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
     </div>
   )
 }
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -8,7 +8,8 @@ const STAGES = ['Start', 'Playing', 'End']
 
 const initialStage = {
   gameStage: STAGES[0],
-  questions
+  questions,
+  currentQuestion: 0
 }
 
 const quizReducer = (state, action) => {
@@ -17,6 +18,14 @@ const quizReducer = (state, action) => {
       return {
         ...state,
         gameStage: STAGES[1]
+      }
+    case 'REORDER_QUESTIONS':
+      const reorderQuestions = questions.sort(() => {
+        return Math.random() - 0.5
+      })
+      return {
+        ...state,
+        questions: reorderQuestions
       }
     default:
       return state
